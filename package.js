b8e732c6d2a1899ed005f03783827a126a19b901
Magesh Vadivelu <mageshkhanna@gmail.com>
2016-07-23 16:40:14 -0700

    Adding logic to blacklist

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mageshkhanna:meteor-username-blacklist',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Blacklist usernames you dont want your users to sign up with.',
   // URL to the Git repository containing the source code for this package.
