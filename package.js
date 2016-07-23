7aeccdf940fc377f82908984258ab9e5ac0603cb
Magesh Vadivelu <mageshkhanna@gmail.com>
2016-07-23 16:34:35 -0700

    Adding desscription and github link

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'mageshkhanna:meteor-username-blacklist',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Blacklist usernames you dont want your users to sign up with.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/magevadi/meteor-username-blacklist',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
