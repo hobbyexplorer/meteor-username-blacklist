1e12ffe477754eeb35353c1e75171547daec580b
Magesh Vadivelu <mageshkhanna@gmail.com>
2016-07-23 17:38:25 -0700

    Adding package changes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mageshkhanna:meteor-username-blacklist',
-  version: '0.1.0',
+  version: '0.2.1',
   // Brief, one-line summary of the package.
   summary: 'Blacklist usernames you dont want your users to sign up with.',
   // URL to the Git repository containing the source code for this package.
@@ -12,12 +12,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3.5.1');
-  api.use('ecmascript');
+  api.use("accounts-password");
   api.mainModule('meteor-username-blacklist.js');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
   api.use('tinytest');
   api.use('mageshkhanna:meteor-username-blacklist');
   api.mainModule('meteor-username-blacklist-tests.js');
